dcca5ab7570a3851e007b13408d8ac06dc011720
David Michael <davidnagib101@gmail.com>
2024-12-03 13:49:12 +0200

    LexicalAnalyzer2 update

--- Phase 1/LexicalAnalyzer2.cpp
+++ Phase 1/LexicalAnalyzer2.cpp
@@ -1,4 +1,5 @@
 #include "LexicalAnalyzer2.h"
+using namespace std;
 
 // TODO:
 // Keep in mind that the code now returns symbo table with the accepting state id's as the token ID (this needs to change)
@@ -7,51 +8,92 @@
 // TODO:
 // Also, this lexical analyzer just fails when encountering invalid symbols, no recovery.
 
-/** Method to analyze input and build the symbol table */
-std::vector<Symbol> LexicalAnalyzer2::analyze(std::vector<char> &input_file)
+/** Method to fill the buffer. Returns true if there are no more characters in the input stream. */
+bool fill_buffer(std::vector<char> &buffer, std::ifstream &ip)
 {
-  std::vector<Symbol> symbol_table;      // The symbol table to return
-  std::vector<char> buffer = input_file;              // Buffer to store the current lexeme
-  int current_state = dfa.get_initial(); // Start with the initial state.
-  int last_token = -1, end_i = 1;         // Track the last accepting state
-  int dead_state = 0;
-  int next_state = 0;
-  size_t last_accepting_pos = 0;         // Position of the last accepting state
-  char c;                                // Character to read
-  size_t i = 0;                          // Position in the buffer
+  char c;
+  int counter = 0;
+  while (counter < BUFFER_SIZE && ip.get(c))
+  {
+    // Skip whitespace
+    if (c == ' ' || c == '\t' || c == '\n' || c == '\r' || c == '\v' || c == '\f') continue;
+    buffer.push_back(c);
+    ++counter;
+  }
+  return counter == 0;
+}
 
-  // Print the accepting states
-  std::cout << "Accepting states: ";
+/** Constructor */
+LexicalAnalyzer2::LexicalAnalyzer2(
+    const DFA &dfa, 
+    const unordered_map<char,char> &mapper,
+    const unordered_map<int, string> &token_names
+  ) : dfa(dfa), mapper(mapper), token_names(token_names) 
+{
+  if (token_names.find(-1) != token_names.end() && token_names.at(-1) != "ERROR") 
+  {
+    throw runtime_error("Token names map must contain an ERROR token.");
+  }
   for (auto const &pair : dfa.get_accepting())
   {
-    std::cout << pair.first << " ";
+    if (token_names.find(pair.second) == token_names.end()) 
+    throw runtime_error("Token names map must contain all token IDs in the DFA.");
   }
-  // print the buffer size
-  std::cout << "Buffer size: " << buffer.size() << std::endl;
-  while (i < buffer.size())
-  {
-    // Get the current character
-    c = buffer.at(i);
+}
 
-    // Get next state
-    next_state = dfa.transition(current_state, this->mapper.at(c));
-    //print the next state
-    std::cout << "Next state: " << next_state << std::endl;
+/** Method to analyze input and build the symbol table */
+vector<Symbol> LexicalAnalyzer2::analyze(ifstream &input_file)
+{
+  vector<Symbol> symbol_table;            // The symbol table to return
+  vector<char> buffer;                    // Buffer to store the current lexeme
+  int current_state = dfa.get_initial();  // Start with the initial state.
+  int last_token = -1;                    // Track the last accepting state
+  int dead_state = this->dfa.get_dead_state();
+  int next_state = 0;
+  size_t i = 0, end_i = 1;                // Position of current element in the buffer and the last accepting element
+  bool eof_flag = false;                  // Flag to indicate end of file (fill_buffer returns false)
+
+  // // Print the accepting states
+  // cout << "Accepting states: ";
+  // for (auto const &pair : dfa.get_accepting())
+  // {
+  //   cout << pair.first << " ";
+  // }
+
+  // // print the buffer size
+  // cout << "Buffer size: " << buffer.size() << endl;
+
+  do 
+  {
+    // Check if the buffer is empty
+    if (i == buffer.size())
+    {
+      // Try to fill the buffer and if there is no more input, set the EOF flag
+      // which will be equivalent to reaching a dead state.
+      eof_flag = fill_buffer(buffer, input_file);
+    }
+
+    // Get next state on inputting the current character. 
+    // If we reach EOF, the next state will be the dead state.
+    next_state = (!eof_flag)? dfa.transition(current_state, this->mapper.at(buffer.at(i))) 
+                            : dead_state;
+    // print the next state
+    //  cout << "Next state: " << next_state << endl;
 
     // If this inputs leads to a dead state, this means that there is no further tokens to be found
     // on starting from the current start character. We either accept some token or mark an error.
     if (next_state == dead_state) 
     {
-      //print the lexeme
-      std::cout << "Lexeme: ";
-      for (size_t j = last_accepting_pos; j < end_i; j++)
-      {
-        std::cout << buffer.at(j);
-      }
+      // //print the lexeme
+      // cout << "Lexeme: ";
+      // for (size_t j = last_accepting_pos; j < end_i; j++)
+      // {
+      //   cout << buffer.at(j);
+      // }
       // Add to the symbol table the last accepted lexeme and token ID
       symbol_table.push_back({
-        std::string(buffer.begin(), buffer.begin() + end_i), // Lexeme, will be one character only if no token was found (end_i = 1)
-        last_token // Will be -1 if no token was found
+        string(buffer.begin(), buffer.begin() + end_i), // Lexeme, will be one character only if no token was found (end_i = 1)
+        this->token_names.at(last_token) // Token name, will be ERROR if no token was found (last token = -1)
       });
       // Delete the lexeme from the buffer
       buffer.erase(buffer.begin(), buffer.begin() + end_i); // Will only erase one character if no token was found
@@ -60,6 +102,8 @@ std::vector<Symbol> LexicalAnalyzer2::analyze(std::vector<char> &input_file)
       end_i = 1;
       current_state = dfa.get_initial();
       i = 0;
+      // Reset EOF flag
+      eof_flag = false;
       continue;
     }
 
@@ -73,7 +117,9 @@ std::vector<Symbol> LexicalAnalyzer2::analyze(std::vector<char> &input_file)
     // increment i and update current state
     ++i;
     current_state = next_state;
-  }
 
+  } while (buffer.size() > 0);
+
+  // Handle remaining characters
   return symbol_table;
 }
--- Phase 1/LexicalAnalyzer2.h
+++ Phase 1/LexicalAnalyzer2.h
@@ -11,15 +11,20 @@ const int BUFFER_SIZE = 256;
 struct Symbol
 {
     std::string lexeme; // The lexeme (token)
-    int token_id;       // Token ID associated with the accepting state
+    std::string token_name;       // Token ID associated with the accepting state
 };
 
 class LexicalAnalyzer2 
 {
   private:
     const DFA &dfa; // Reference to the DFA used for analysis
-    const std::unordered_map<char, char> mapper;
+    const std::unordered_map<char, char> &mapper;
+    const std::unordered_map<int, std::string> &token_names;
   public:
-    LexicalAnalyzer2(const DFA &dfa, const std::unordered_map<char,char> &mapper) : dfa(dfa), mapper(mapper) {}
-    std::vector<Symbol> analyze(std::vector<char> &input_file);
+    LexicalAnalyzer2(
+      const DFA &dfa, 
+      const std::unordered_map<char,char> &mapper,
+      const std::unordered_map<int, std::string> &token_names
+    );
+    std::vector<Symbol> analyze(std::ifstream &input_file);
 };
